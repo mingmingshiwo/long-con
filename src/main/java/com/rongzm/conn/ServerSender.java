99f6c7715b09056b3c86b4cce6d006c540b8426b
Ming <84724908@qq.com>
2014-10-21 17:38:51 +0800

    see

--- src/main/java/com/rongzm/conn/ServerSender.java
+++ src/main/java/com/rongzm/conn/ServerSender.java
@@ -1,6 +1,13 @@
 package com.rongzm.conn;
 
 public interface ServerSender {
+	/**
+	 * @param id
+	 * @param msg
+	 */
 	public void send(int id,String msg);
+	/**
+	 * @param msg
+	 */
 	public void broadcast(String msg);
 }
